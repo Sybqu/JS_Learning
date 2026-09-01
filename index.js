5657112fe78c029496f75bd709fa50c781344183
Sybqu <garrishtbhargava@gmail.com>
2026-09-01 20:28:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,5 +6,59 @@
 /*multi
 line comment maxxing */
 
+// document.getElementById("heading1").textContent = 'Hello';
+// document.getElementById("random-para?").textContent = " Who is a good kitten?"
+
+//  Variables
+// declation = let ;
+let x;
+x = 100;
+
+console.log(x);
+let age = 19;
+let gpa = 9.77 ;
+let claude_max_subscriptions = 0;
+
+console.log(`im ${age} years old my gpa is ${gpa} HAHA and i have ${claude_max_subscriptions} claude_max_subscriptions`);
+console.log(typeof gpa);
+
+let FirstStringInJs = "yO fIrEst Ever String!?!";
+console.log(typeof FirstStringInJs);
+console.log("MMm this is my first string in js?" + FirstStringInJs);
+// booleans we dont needna go over em do we 
+// arithmetic slop 
+
+//accepting user input
+let username;
+
+document.getElementById("usr").onclick = function () {
+    username = document.getElementById("txt").value;
+    console.log(username);
+    document.getElementById("random-para?").textContent = "Logged in as " + username
+
+}
+
+// type conversion FUck yes WE HAVE ALWAYSSSSSSS SKIPPED TYPE CONVERSION
+// constants SLop
 
 
+// Counter Program:
+const decrease = document.getElementById("b3");
+const reset = document.getElementById("b2");
+const increase = document.getElementById("b1");
+const contlabel = document.getElementById("counter");
+let count = 0;
+
+increase.onclick = function(){
+    count++;
+    contlabel.textContent = count;
+}
+
+decrease.onclick = function(){
+    count--;
+    contlabel.textContent = count;
+}
+reset.onclick = function(){
+    count = 0 ;
+    contlabel.textContent = count;
+}
